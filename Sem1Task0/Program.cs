337ce930b34687e9d6bf1408570ffba81b7ccadb
ANASTASIA <nastik.serjantova@gmail.com>
2023-09-17 17:06:48 +0300

    solution from hom 2 last task

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Введите число: ");
+//Считываем данные с консоли
+string inputNum = Console.ReadLine();
+//Проверим ,чтобы данные были не пустые
+if(inputNum!=null)
+{
+   //Просим введенное число
+   int num = int.Parse(inputNum);
+   //Находим квадрат числа
+   int res = num*num;
+   //Вводим данные в консоль
+   Console.WriteLine(res);
+}
+
+Console.WriteLine("Квадрат числа: "+(int)Math.Pow(int.Parse(inputNum),2));
+
